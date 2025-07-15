5e62cb27906add459c3b2b316eb9553da83c66cf
lilbunnymation <snowylulu11@gmail.com>
2025-07-15 14:40:34 +0300

    Create app.py

---
+++ app.py
@@ -0,0 +1,46 @@
+!pip install openai
+!pip install git+https://github.com/openai/whisper.git
+!pip install pytube
+import whisper
+from pytube import YouTube
+from google.colab import files
+import os
+model= whisper.load_model("base")
+yt_url = input("Paste a youtube link here!:")
+
+def download_youtube_audio(url) :
+  yt = youtube(url)
+  stream = yt.streams.filter(only_audio=True).first()
+  stream.download(filename="yt_audio.mp3")
+  return "yt_audio.mp3"
+if yt_url:
+  filename = download_youtube_audio(yt_url)
+
+result = model.transcribe(filename)
+transcript = result["text"]
+print("📜 Transcript:\n")
+print(transcript)
+
+import openai
+openai.api_key = "your-openai-api-key"
+
+def generate_notes(text):
+  prompt = f"summarize these lecture notes clearly and simply:\n{text}"
+  response = openai.ChatCompletion.create(
+      model="gpt-3.5-turbo" ,
+      messages=[{"role": "user","content": prompt}]
+  )
+  return response['choices'][0]['message']['content']
+
+notes = generate_notes(transcript)
+print(notes)
+def generate_flashcards(text):
+  prompt = f"make 10 flashcards from this content in Q: A: format.\n{text}"
+  response = openai.ChatCompletion.create(
+      model="gpt-3.5-turbo",
+      messages=[{"role": "user", "content": prompt}]
+  )
+  return response['choices'][0]['message']['content']
+
+flashcards = generate_flashcards(transcript)
+print(flashcards)
