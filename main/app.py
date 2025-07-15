1f2e35ef7866349f056f73ed2cb3bbeeb35a7b4c
lilbunnymation <snowylulu11@gmail.com>
2025-07-15 16:35:45 +0300

    Update app.py

--- main/app.py
+++ main/app.py
@@ -1,42 +1,39 @@
-import whisper
+import streamlit as st
 from pytube import YouTube
+import openai
 import os
-model= whisper.load_model("base")
-yt_url = input("Paste a youtube link here!:")
 
-def download_youtube_audio(url) :
-  yt = youtube(url)
+openai.api_key = "your-openai-api-key"
+st.title("🎓 NoteSnap")
+st.subheader("Turn a YouTube video into notes + flashcards!")
+yt_url = st.text_input("📺 Paste a YouTube video link here:")
+def download_youtube_audio(url):
+  yt = YouTube(url)
   stream = yt.streams.filter(only_audio=True).first()
   stream.download(filename="yt_audio.mp3")
   return "yt_audio.mp3"
 if yt_url:
-  filename = download_youtube_audio(yt_url)
-
-result = model.transcribe(filename)
-transcript = result["text"]
-print("📜 Transcript:\n")
-print(transcript)
-
-import openai
-openai.api_key = "your-openai-api-key"
-
-def generate_notes(text):
-  prompt = f"summarize these lecture notes clearly and simply:\n{text}"
-  response = openai.ChatCompletion.create(
-      model="gpt-3.5-turbo" ,
-      messages=[{"role": "user","content": prompt}]
-  )
-  return response['choices'][0]['message']['content']
-
-notes = generate_notes(transcript)
-print(notes)
-def generate_flashcards(text):
-  prompt = f"make 10 flashcards from this content in Q: A: format.\n{text}"
-  response = openai.ChatCompletion.create(
-      model="gpt-3.5-turbo",
-      messages=[{"role": "user", "content": prompt}]
-  )
-  return response['choices'][0]['message']['content']
-
-flashcards = generate_flashcards(transcript)
-print(flashcards)
+  with st.spinner("🎧 Downloading and transcribing..."):
+    filename = download_youtube_audio(yt_url)
+    st.sbheader("📝 Transcript")
+    st.text_area("Transript:", transcript, height=300)
+    def generate_notes(text):
+      prompt = f"summarize these lecture notes clearly and simply:\n{text}"
+      response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo"
+        messages=[{"role":"user", "content": prompt}]
+      )
+      return response ['choices'][0]['message']['content']
+    notes = generate_notes(transcript)
+    st.subheader("📚 Study Notes")
+    st.markdown(notes)
+    def generate_flashcards(text):
+      prompt = f"make 10 flashcards from this content in Q: A: format:\n{text}"
+      response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[{"role": "user", "content": prompt}]
+      )
+      return response['choices'][0]['message']['content']
+    flashcards = generate_flashcards(transcript)
+    st.subheader("Flashcards!")
+    st.text_area("Flashcards:", flashcards, height=300)
